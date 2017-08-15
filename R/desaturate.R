c52a69cda64822b4b00ac92d1bbabf43efca6890
Claus Wilke <wilke@austin.utexas.edu>
2017-08-15 10:47:11 -0500

    fix bug, add feature

--- R/desaturate.R
+++ R/desaturate.R
@@ -1,5 +1,5 @@
 ## desaturate colors (remove chroma in HCL space)
-desaturate <- function(col) {
+desaturate <- function(col, severity = 1) {
   ## col has to be hex code, otherwise col2rgb is used
   if(is.character(col) &&
     (all(substr(col, 1L, 1L) == "#") & all(nchar(col) %in% c(7L, 9L))))
@@ -16,12 +16,14 @@ desaturate <- function(col) {
     alpha <- format(as.hexmode(col[4L, ]), width = 2L, upper.case = TRUE)
     alpha[alpha == "FF"] <- ""
     ## retain only RGB
-    col <- RGB(t(col[1L:3L, ])/255)
+    # col <- RGB(t(col[1L:3L, ])/255) # this call is incorrect, needs to be sRGB
+    col <- sRGB(t(col[1L:3L, ])/255) # this call is incorrect, needs to be sRGB
+
   }
   
   ## convert to HCL and remove chroma
   col <- as(col, "polarLUV")
-  col@coords[, 2L] <- 0
+  col@coords[, 2L] <- (1-severity)*col@coords[, 2L]
   
   ## fix-up extreme luminance cases
   col@coords[col@coords[, 1L] <= 0 | col@coords[, 1L] >= 100, 2L:3L] <- 0
